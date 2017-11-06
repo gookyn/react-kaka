c164ab94a25813ed087729578af92c0e2cece124
liujinge <18404985936@163.com>
2017-11-06 19:48:14 +0800

    init

---
+++ src/pages/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+	render() {
+		return(
+			<div id="app">
+				<p className="t-c">App</p>
+      		</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+	render() {
+		return (
+			<div class="login">
+				login
+			</div>
+		)
+	}
+}
+
+export default Login;
